e5bb0a8fa00f651af3a6c08476074cef149a9c61
SLeBrasseur <lb_samuel@hotmail.com>
2015-11-03 15:13:49 -0500

    Create rech_div.m

---
+++ rech_div.m
@@ -0,0 +1,40 @@
+
+
+function [ x, y ] = rech_div(tableau)
+% Fonction qui met une date selon un format désiré.
+% [ x, y ] = rech_div(tableau)
+%*************************************************
+% Nom du fichier: rech_div.m
+% Description: exercice 4 
+% Auteur: Samuel Le Brasseur
+%         Juliette Tibayrenc
+% Paramètres: éléments paires, éléments divisibles par 3
+% Valeurs de retour: nombre???
+% Date: 3 novembre
+%***************************************
+
+
+if nargin~=1
+    %erreur
+    fprintf('erreur paramètre d''entrée \n');
+else 
+    
+   n=length (tableau)
+   for i = 1:n
+     if (mod(tableau (i) ,2))==0
+     
+   m= length(x)
+   x(m+1)= tableau(i);
+   
+   
+    if (mod(tableau ,3))
+   end
+   %%si nargin == 1
+   % x= (mod(tableau ,2));
+   % y= (mod(tableau ,3));
+   %[x,y]= rech_div([1 2 3 4 5 6 7 8 9 ])
+   
+end
+
+
+end
